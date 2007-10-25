77bd4c124ff75cac07ac8272ddc52327fe92258e
Quinn Jensen <quinn.jensen@freescale.com>
2007-10-24 21:18:52 -0600

    Add MX27, MX31, and MX32 splash screens to kernel.
    Add MX27, MX31, and MX32 splash screens to the linux 2.6.22 kernel.
    Graphics images created by Ross Wille
    Tux logo created by Larry Ewing (http://www.isc.tamu.edu/~lewing/linux/)
    
    http://www.bitshrine.org/gpp/linux-2.6.22-mx-Add-MX27-MX31-and-MX32-splash-screens-to-2.patch

--- drivers/video/logo/logo.c
+++ drivers/video/logo/logo.c
@@ -26,6 +26,9 @@ extern const struct linux_logo logo_linux_vga16;
 extern const struct linux_logo logo_linux_clut224;
 extern const struct linux_logo logo_dec_clut224;
 extern const struct linux_logo logo_mac_clut224;
+extern const struct linux_logo logo_mx27ads_clut224;
+extern const struct linux_logo logo_mx31ads_clut224;
+extern const struct linux_logo logo_mx32ads_clut224;
 extern const struct linux_logo logo_parisc_clut224;
 extern const struct linux_logo logo_sgi_clut224;
 extern const struct linux_logo logo_sun_clut224;
@@ -33,7 +36,6 @@ extern const struct linux_logo logo_superh_mono;
 extern const struct linux_logo logo_superh_vga16;
 extern const struct linux_logo logo_superh_clut224;
 extern const struct linux_logo logo_m32r_clut224;
-
 
 const struct linux_logo *fb_find_logo(int depth)
 {
@@ -77,6 +79,18 @@ const struct linux_logo *fb_find_logo(int depth)
 		/* Macintosh Linux logo on m68k */
 		if (MACH_IS_MAC)
 			logo = &logo_mac_clut224;
+#endif
+#ifdef CONFIG_LOGO_MX27ADS_CLUT224
+		/* Freescale i.MX27ADS Linux logo */
+		logo = &logo_mx27ads_clut224;
+#endif
+#ifdef CONFIG_LOGO_MX31ADS_CLUT224
+		/* Freescale i.MX31ADS Linux logo */
+		logo = &logo_mx31ads_clut224;
+#endif
+#ifdef CONFIG_LOGO_MX32ADS_CLUT224
+		/* Freescale i.MX32ADS Linux logo */
+		logo = &logo_mx32ads_clut224;
 #endif
 #ifdef CONFIG_LOGO_PARISC_CLUT224
 		/* PA-RISC Linux logo */
