f75cb71653c308e69cd6ec3ed4c00ccfc3c2249e
Anthony Felice <tony.felice@timesys.com>
2015-06-24 16:10:13 -0400

    arm: imx: Add backwards-compatibility support for MCC 1.x.
    Signed-off-by: Anthony Felice <tony.felice@timesys.com>

---
+++ include/uapi/linux/mvf_sema4.h
@@ -0,0 +1,12 @@
+#ifndef __UAPI_MVF_SEMA4__
+#define __UAPI_MVF_SEMA4__
+
+#include <linux/sched.h>
+
+#define MVF_SHMEM_SEMAPHORE_NUMBER      (1)
+#define MVF_PRINTF_SEMAPHORE_NUMBER     (2)
+#define MVF_I2C_SEMAPHORE_NUMBER        (3)
+#define MVF_RESERVED1_SEMAPHORE_NUMBER  (4)
+#define MVF_RESERVED2_SEMAPHORE_NUMBER  (5)
+
+#endif /* __UAPI_MVF_SEMA4__ */
